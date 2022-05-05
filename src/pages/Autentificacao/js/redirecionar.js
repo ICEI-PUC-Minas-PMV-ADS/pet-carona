c5a6f7182b0e004a49ed5bb1bbeaf6bb475b2811
Igor Goncalo <igorlabakig@gmail.com>
2022-05-05 23:03:23 +0100

    Comentarios

--- src/pages/Autentificacao/js/redirecionar.js
+++ src/pages/Autentificacao/js/redirecionar.js
@@ -1,20 +1,26 @@
+/**
+ * Constantes
+ */
 const buttonAuth   = document.getElementById('botao-autentificacao');
 
-
+/**
+ * Funcao dinamica para redirecionar a url
+ */
 function redirect(url){
     window.location.href = url;
 }
 
+/**
+ * Condicao para definir a url de acordo com o modo do form
+ */
 if(buttonAuth.innerHTML.includes('ENTRAR')){
     buttonAuth.addEventListener('click',(e) => {
         e.preventDefault();
-        console.log('porra');
         redirect("http://127.0.0.1:5500/src/pages/Cadastro");
     })
 }else{
     buttonAuth.addEventListener('click',(e) => {
         redirect("http://127.0.0.1:5500/src/pages/Cadastro");
-        console.log('porra');
     })
 }
 

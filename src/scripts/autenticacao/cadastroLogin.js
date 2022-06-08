ef5ac0e056a4f87e90fd68acbd3720faf2732c10
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-06-08 17:37:17 -0300

    formatacao

--- src/scripts/autenticacao/cadastroLogin.js
+++ src/scripts/autenticacao/cadastroLogin.js
@@ -1,65 +1,68 @@
-const buttonAuth   = document.getElementById('botao-autentificacao');
+const buttonAuth = document.getElementById('botao-autentificacao');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('senha');
 const nameInput = document.getElementById('nome');
 let usuarioAtual = {};
 
-if (!(localStorage.getItem('usuarios'))) {
-    // Preenchimento inicial do localStorage para o primeiro acesso
-    const dadosIniciais = {
-        usuarios: [
-            {id: '1', nome: 'João Silva', endereco: 'Rua Pereira Neto, 456', bairro: 'Mooca', CEP: '01200-020', funcao: 'motorista'}
-        ]
-    }
-    localStorage.setItem('usuarios', JSON.stringify (dadosIniciais));
-};
+if (!localStorage.getItem('usuarios')) {
+	// Preenchimento inicial do localStorage para o primeiro acesso
+	const dadosIniciais = {
+		usuarios: [
+			{
+				id: '1',
+				nome: 'João Silva',
+				endereco: 'Rua Pereira Neto, 456',
+				bairro: 'Mooca',
+				CEP: '01200-020',
+				funcao: 'motorista',
+			},
+		],
+	};
+	localStorage.setItem('usuarios', JSON.stringify(dadosIniciais));
+}
 
 let dbUsuarios = JSON.parse(localStorage.getItem('usuarios'));
 
-function redirect(url){
-    window.location.href = url;
+function redirect(url) {
+	window.location.href = url;
 }
 
-buttonAuth.addEventListener('click',(e) => {
-    e.preventDefault();
-    if(buttonAuth.innerHTML.includes('ENTRAR')) {
-        if (emailInput === 'teste@gmail.com' && passwordInput === 'teste') {
-            redirect('PerfilPassageiro.html')
-        }
-    } else {
-        let emailCadastro = emailInput.value;
-        for (let i = 0; i < dbUsuarios.usuarios.length; i++) {
-            let usuario = dbUsuarios.usuarios[i];
-            if (usuario.email === emailCadastro) {
-                return alert('Já existe um usuário com esse email.');
-            }
-        }
-        addUser(nameInput.value, emailInput.value, passwordInput.value)
-    }
+buttonAuth.addEventListener('click', (e) => {
+	e.preventDefault();
+	if (buttonAuth.innerHTML.includes('ENTRAR')) {
+		loginUser(emailInput.value, passwordInput.value);
+	} else {
+		let emailCadastro = emailInput.value;
+		for (let i = 0; i < dbUsuarios.usuarios.length; i++) {
+			let usuario = dbUsuarios.usuarios[i];
+			if (usuario.email === emailCadastro) {
+				return alert('Já existe um usuário com esse email.');
+			}
+		}
+		addUser(nameInput.value, emailInput.value, passwordInput.value);
+	}
 });
 
 // Login ou cadastro
-function loginUser (email, senha) {
-    for (let i = 0; i < dbUsuarios.usuarios.length; i++) {
-        let usuario = dbUsuarios.usuarios[i];
-        if (email === usuario.email && senha === usuario.senha) {
-            usuarioAtual.email = usuario.email;
-            usuarioAtual.nome = usuario.nome;
-        }
-
-        sessionStorage.setItem('usuarioAtual', JSON.stringify (usuarioAtual));
-        return true;
-    }
-
-    return false;
+function loginUser(email, senha) {
+	for (let i = 0; i < dbUsuarios.usuarios.length; i++) {
+		let usuario = dbUsuarios.usuarios[i];
+		if (email === usuario.email && senha === usuario.senha) {
+			usuarioAtual.email = usuario.email;
+			usuarioAtual.nome = usuario.nome;
+		}
+		sessionStorage.setItem('usuarioAtual', JSON.stringify(usuarioAtual));
+		redirect('PerfilPassageiro.html');
+	}
+	return alert('Senha ou email incorretos.');
 }
 
-function addUser (nome, email, senha) {
-    let novoUsuario = {
-        nome: nome,
-        email: email,
-        senha: senha
-    };
-    dbUsuarios.usuarios.push(novoUsuario);
-    localStorage.setItem('usuarios', JSON.stringify(dbUsuarios));
+function addUser(nome, email, senha) {
+	let novoUsuario = {
+		nome: nome,
+		email: email,
+		senha: senha,
+	};
+	dbUsuarios.usuarios.push(novoUsuario);
+	localStorage.setItem('usuarios', JSON.stringify(dbUsuarios));
 }

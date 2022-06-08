568289f642f58056eab16e9525c31da9c09b92dc
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-06-08 14:45:08 -0300

    testing redirect github pages

--- src/scripts/autenticacao/redirecionar.js
+++ src/scripts/autenticacao/redirecionar.js
@@ -4,30 +4,21 @@
 const buttonAuth   = document.getElementById('botao-autentificacao');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('senha');
-// const nameInput = document.getElementById('nome');
-/**
- * Funcao dinamica para redirecionar a url
- */
+const nameInput = document.getElementById('nome');
+
 function redirect(url){
     window.location.href = url;
 }
 
-/**
- * Condicao para definir a url de acordo com o modo do form
- */
-if(buttonAuth.innerHTML.includes('ENTRAR')) {
-    buttonAuth.addEventListener('click',(e) => {
-        e.preventDefault();
+buttonAuth.addEventListener('click',(e) => {
+    e.preventDefault();
+    if(buttonAuth.innerHTML.includes('ENTRAR')) {
         if (emailInput.value === 'teste@gmail.com' && passwordInput.value === 'teste') {
             redirect('PerfilPassageiro.html')
         }
-    })
-} 
-// else {
-//     buttonAuth.addEventListener('click',(e) => {
-//         if (emailInput.value && passwordInput.value) {
-//             redirect('Cadastro.html')
-//         }
-//     })
-// }
+    } else {
+        console.log('teste')
+        redirect('Cadastro.html')
+    }
+})
 

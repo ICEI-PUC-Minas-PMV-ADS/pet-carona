f1b62de73602a82a108dafa4b2475b60d9298662
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-06-10 16:47:53 -0300

    ajuste bug ediçao perfil

--- src/scripts/editarPerfilPassageiro.js
+++ src/scripts/editarPerfilPassageiro.js
@@ -1,39 +1,40 @@
 // TODO: turn gender radio
+
 let editButton = document.getElementById('edit-button');
 const editableFields = document.getElementsByClassName('editable');
-
-// Atualização do localStorage conforme modificações feitas
 let usuarioAtual = JSON.parse(sessionStorage.getItem('usuarioAtual'));
 document.getElementById('nomeUsuario').innerHTML = usuarioAtual.nome;
 document.getElementById('enderecoUsuario').innerHTML = usuarioAtual.endereco;
 document.getElementById('bairroUsuario').innerHTML = usuarioAtual.bairro;
 document.getElementById('CEPUsuario').innerHTML = usuarioAtual.CEP;
 
-let editions = 0;
-
 // Edição das informações contidas nos campos
 let editable = false;
 
-let updateStorage = function () {
+function updateStorage() {
+	let nomeAtualizado = document.getElementById('nomeUsuario').innerHTML;
+	let enderecoAtualizado = document.getElementById('enderecoUsuario').innerHTML;
+	let bairroAtualizado = document.getElementById('bairroUsuario').innerHTML;
+	let CEPAtualizado = document.getElementById('CEPUsuario').innerHTML;
 	let usuarioAtualizado = {
 		...usuarioAtual,
-		nome: document.getElementById('nomeUsuario').innerHTML,
-		endereco: document.getElementById('enderecoUsuario').innerHTML,
-		bairro: document.getElementById('bairroUsuario').innerHTML,
-		CEP: document.getElementById('CEPUsuario').innerHTML,
+		nome: nomeAtualizado,
+		endereco: enderecoAtualizado,
+		bairro: bairroAtualizado,
+		CEP: CEPAtualizado,
 	};
 	console.log(usuarioAtualizado);
 	sessionStorage.setItem('usuarioAtual', JSON.stringify(usuarioAtualizado));
-};
+}
 
 let turnEditable = function () {
+	updateStorage();
 	editable = !editable;
 	if (editable) {
 		editButton.innerHTML = 'Salvar';
 		for (let element of editableFields) {
 			element.className = 'editable editing';
 			element.contentEditable = true;
-			editions += 1;
 		}
 	} else {
 		editButton.innerHTML = 'Editar perfil';
@@ -41,7 +42,6 @@ let turnEditable = function () {
 			element.className = 'editable';
 			element.contentEditable = false;
 		}
-		if (editions !== 0) updateStorage();
 	}
 };
 

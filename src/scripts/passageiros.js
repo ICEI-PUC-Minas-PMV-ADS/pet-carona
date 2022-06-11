92ad0077182789e8c26163e547b0ce07de0e1612
Jaynne <103083123+jnunesss@users.noreply.github.com>
2022-06-10 22:52:19 -0300

    integracao lista passageiros e motoristas

---
+++ src/scripts/passageiros.js
@@ -0,0 +1,50 @@
+let passageiros = [{
+    nome: 'João Silva',
+    avaliacao: '4.9',
+    viagens: '13',
+    perfil: 'passageiro',
+    modeloCarro: 'Fiat',
+    tipoCarro: 'SUV',
+    portePet: 'P',
+    ajudaCusto: '10 reais'
+},
+{
+    nome: 'Maria Paula',
+    avaliacao: '4.1',
+    viagens: '13',
+    perfil: 'passageiro',
+    modeloCarro: 'Fiat',
+    tipoCarro: 'SUV',
+    portePet: 'P',
+    ajudaCusto: '10 reais'
+},
+{
+    nome: 'João Silva',
+    avaliacao: '4.9',
+    viagens: '13',
+    perfil: 'passageiro',
+    modeloCarro: 'Fiat',
+    tipoCarro: 'SUV',
+    portePet: 'P',
+    ajudaCusto: '10 reais'
+},
+{
+    nome: 'Celso Ramos',
+    avaliacao: '5.0',
+    viagens: '13',
+    perfil: 'passageiro',
+    modeloCarro: 'Fiat',
+    tipoCarro: 'SUV',
+    portePet: 'P',
+    ajudaCusto: '10 reais'
+},
+{
+    nome: 'Felicia Alves',
+    avaliacao: '4.0',
+    viagens: '13',
+    perfil: 'passageiro',
+    modeloCarro: 'Fiat',
+    tipoCarro: 'SUV',
+    portePet: 'P',
+    ajudaCusto: '10 reais'
+}]
